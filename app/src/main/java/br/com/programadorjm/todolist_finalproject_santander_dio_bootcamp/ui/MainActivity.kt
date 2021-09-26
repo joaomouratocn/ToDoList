d58939601e90a381a2ba77fcb1fdf2218c3a30ad
João Mourato <joaomouratocn@gmail.com>
2021-09-26 20:37:55 -0300

    Criado UI

--- app/src/main/java/br/com/programadorjm/todolist_finalproject_santander_dio_bootcamp/ui/MainActivity.kt
+++ app/src/main/java/br/com/programadorjm/todolist_finalproject_santander_dio_bootcamp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.programadorjm.todolist_finalproject_santander_dio_bootcamp
+package br.com.programadorjm.todolist_finalproject_santander_dio_bootcamp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.programadorjm.todolist_finalproject_santander_dio_bootcamp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
