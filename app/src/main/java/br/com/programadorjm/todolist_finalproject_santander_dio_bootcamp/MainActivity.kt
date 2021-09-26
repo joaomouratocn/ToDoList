59c02d7271807a6fb624d111d48d6cec52fe9ffc
João Mourato <joaomouratocn@gmail.com>
2021-09-26 16:52:02 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/programadorjm/todolist_finalproject_santander_dio_bootcamp/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.programadorjm.todolist_finalproject_santander_dio_bootcamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
